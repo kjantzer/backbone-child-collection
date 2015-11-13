980100460526b22ea53b8c38f820448bffe5fb48
Kevin Jantzer <kevin@kevinjantzer.com>
2015-11-13 09:11:14 -0800

    Uses model attribute data if applicable

--- backbone.childcollection.js
+++ backbone.childcollection.js
@@ -1,10 +1,12 @@
 /*
-	Backbone Child Collection 0.1.0
+	Backbone Child Collection 0.2.0
 	
 	Used for REST collection that is child of a parent model.
 	
 	@author Kevin Jantzer, Blackstone Audio
 	@since 2015-07-08
+	
+	https://github.com/kjantzer/backbone-child-collection
 	
 	TODO
 	- what happenns if the parentModel is new? should collection not fetch/save?
@@ -81,7 +83,8 @@ _.extend(Backbone.Model.prototype, {
 	// Overrides default to get a collection if no attribute for given `key` exists
 	get: function(key){
 		
-		if( this.attributes[key] != null || !this.collections || !this.collections[key] )
+		// no collection found for key, return model attribute like normal
+		if( !this.collections || !this.collections[key] )
 			return Backbone.Model.prototype._get.apply(this, arguments)
 		
 		return this.getCollection(key);
@@ -95,28 +98,39 @@ _.extend(Backbone.Model.prototype, {
 		if( this.__childCollections[key] )
 			return this.__childCollections[key];
 		
+		// get the collection info for setup and determine if a Collection was given
 		var CollInfo = this.collections && this.collections[key];
 		var CollGiven = CollInfo && CollInfo.prototype && CollInfo.prototype.toJSON && CollInfo.prototype.fetch;
 		
+		// whoops, couldn't find a collection for the given key
 		if( !CollInfo ){
 			console.warn('Collection for `'+key+'` not set.',  this.collections)
 			return null;
 		}
 		
+		// get the "class" of Collection to instantiate
 		var ChildColl = CollGiven ? CollInfo : (CollInfo.collection ? CollInfo.collection : Backbone.ChildCollection);
 		
 		var opts = {
 			parentModel: this
 		}
 		
+		// if the value was a string, it should be the 'urlPath'
 		if( typeof CollInfo === 'string' )
 			opts.urlPath = CollInfo;
-			
+		
+		// else, if an object, and not a Collection, pass everything as options to the collection
 		else if( !CollGiven && typeof CollInfo === 'object')
 			opts = _.extend(CollInfo, opts);
-			
-		var Coll = this.__childCollections[key] = new ChildColl([], opts)
 		
+		// get bootstrapped model data for initial creation of collection.
+		// if the key of this collection matches a model attribute (and its an array), assume they're models
+		var models = (this.attributes[key] && _.isArray(this.attributes[key]) && this.attributes[key]) || [];
+		
+		// create and store reference to this collection
+		var Coll = this.__childCollections[key] = new ChildColl(models, opts)
+		
+		// make sure parent model is set on the collection
 		Coll.parentModel = this;
 		
 		return Coll;
