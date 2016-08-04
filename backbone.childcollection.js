58f17a9c6df9d17e8978f1017e10131b95a9bfff
Kevin Jantzer <kevin@kevinjantzer.com>
2016-08-04 09:25:16 -0700

    `getOrFetch`: better ‘success’ callback support when multiple calls for the same model

--- backbone.childcollection.js
+++ backbone.childcollection.js
@@ -1,5 +1,5 @@
 /*
-    Backbone Child Collection 0.7.1
+    Backbone Child Collection 0.7.2
 	
 	Used for REST collection that is child of a parent model.
 	
@@ -13,6 +13,7 @@
 	- if the model is destroyed, we should probably clean up all child collections
 */
 
+// TODO: let `id` be a hash of attributes
 Backbone.Collection.prototype.getOrCreate = function(id){
 
     if( !id )
@@ -52,7 +53,7 @@ Backbone.Collection.prototype.getOrFetch = function(id, success){
 
     var model = this.getOrCreate.apply(this, arguments)
 
-    // only one means we just created this model
+    // model has not fetched yet (via getOrFetch)
     if( model.needsFetching ){
 
         delete model.needsFetching;
@@ -61,7 +62,20 @@ Backbone.Collection.prototype.getOrFetch = function(id, success){
         model.fetch({success: function(){
             delete model.isFetching
             if( success ) success(model)
+
+            // are there any other success callbacks waiting? Call them now (see below)
+            if( model._getOrFetchSuccess ){
+                _.each(model._getOrFetchSuccess, function(fn){ fn(model) });
+                delete model._getOrFetchSuccess;
+            }
         }});
+
+    // model currently fetching, stash the "success" callback on the model - it will be called when fetch completes
+    }else if( model.isFetching && success){
+        model._getOrFetchSuccess = model._getOrFetchSuccess || [];
+        model._getOrFetchSuccess.push(success)
+
+    // already fetched
     }else{
         if( success ) success(model)
     }
@@ -278,7 +292,10 @@ _.extend(Backbone.Model.prototype, {
 	},
 
     _childModelFetched: function(key, model){
-        this.trigger('model:'+key+':fetch', model)
+        // delay to allow for the model to save: `this.__childModels[key] = Model;`
+        _.defer(function(){
+            this.trigger('model:'+key+':fetch', model)
+        }.bind(this))
     },
 
 	_set: Backbone.Model.prototype.set,
